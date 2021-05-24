a0c0fcc0ec748e3b8a0d6c4616c3cadc71b4f17d
Sueda Zehra Ergül <83866353+suedazehra@users.noreply.github.com>
2021-05-24 20:03:37 +0300

    Add files via upload

---
+++ daire_alan_hesapla.py
@@ -0,0 +1,6 @@
+def daire_alan_hesapla:
+    yaricap = float(input("Dairenin yaricapini giriniz = "))
+    alan = (3.14 * yaricap * yaricap)
+    print("Dairenin alani = " , alan )
+daire_alan_hesapla()
+
